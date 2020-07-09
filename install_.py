3c4db95f6ff01fa4cc36926e499626a688ad581f
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2020-07-09 09:58:58 +0530

    trh

--- install_.py
+++ install_.py
@@ -2,20 +2,20 @@
 from google.colab import drive
 drive.mount('/content/drive')
 
-!pip3 install --upgrade moviepy
-!pip3 install dash
-!pip3 install dash_core_components
-!pip3 install dash_html_components
-!pip3 install dash_bootstrap_components
-!pip3 install flask
-!pip3 install youtube_dl
-!pip3 install mhyt
-!pip3 install dash_daq
+get_ipython().system_raw('pip3 install --upgrade moviepy')
+get_ipython().system_raw('pip3 install dash')
+get_ipython().system_raw('pip3 install dash_core_components')
+get_ipython().system_raw('pip3 install dash_html_components')
+get_ipython().system_raw('pip3 install dash_bootstrap_components')
+get_ipython().system_raw('pip3 install flask')
+get_ipython().system_raw('pip3 install youtube_dl')
+get_ipython().system_raw('pip3 install mhyt')
+get_ipython().system_raw('pip3 install dash_daq')
 Mode = "install" 
 
 from pathlib import Path
 if (Mode == "install"):
-  !git clone https://github.com/iperov/DeepFaceLab.git
+  get_ipython().system_raw('git clone https://github.com/iperov/DeepFaceLab.git')
 
   # fix linux warning
   # /usr/lib/python3.6/multiprocessing/semaphore_tracker.py:143: UserWarning: semaphore_tracker: There appear to be 1 leaked semaphores to clean up at shutdown
@@ -29,28 +29,28 @@ if (Mode == "install"):
   fin.close()
 else:
   %cd /content/DeepFaceLab
-  !git pull
+  get_ipython().system_raw('git pull')
 
-!pip install -r /content/DeepFaceLab/requirements-colab.txt
-!pip install --upgrade scikit-image
-!apt-get install cuda-10-0
+get_ipython().system_raw('pip install -r /content/DeepFaceLab/requirements-colab.txt')
+get_ipython().system_raw('pip install --upgrade scikit-image')
+get_ipython().system_raw('apt-get install cuda-10-0')
 
 if not Path("/content/pretrain").exists():
   print("Downloading CelebA faceset ... ")
-  !wget -q --no-check-certificate -r 'https://github.com/chervonij/DFL-Colab/releases/download/pretrain-CelebA/pretrain_CelebA.zip' -O /content/pretrain_CelebA.zip
-  !mkdir /content/pretrain
-  !unzip -q /content/pretrain_CelebA.zip -d /content/pretrain/
-  !rm /content/pretrain_CelebA.zip
+  get_ipython().system_raw('wget -q --no-check-certificate -r 'https://github.com/chervonij/DFL-Colab/releases/download/pretrain-CelebA/pretrain_CelebA.zip' -O /content/pretrain_CelebA.zip')
+  get_ipython().system_raw('mkdir /content/pretrain')
+  get_ipython().system_raw('unzip -q /content/pretrain_CelebA.zip -d /content/pretrain/')
+  get_ipython().system_raw('rm /content/pretrain_CelebA.zip')
 
 if not Path("/content/pretrain_Q96").exists():
   print("Downloading Q96 pretrained model ...")
-  !wget -q --no-check-certificate -r 'https://github.com/chervonij/DFL-Colab/releases/download/Q96_model_pretrained/Q96_model_pretrained.zip' -O /content/pretrain_Q96.zip
-  !mkdir /content/pretrain_Q96
-  !unzip -q /content/pretrain_Q96.zip -d /content/pretrain_Q96/
-  !rm /content/pretrain_Q96.zip
+  get_ipython().system_raw('wget -q --no-check-certificate -r 'https://github.com/chervonij/DFL-Colab/releases/download/Q96_model_pretrained/Q96_model_pretrained.zip' -O /content/pretrain_Q96.zip')
+  get_ipython().system_raw('mkdir /content/pretrain_Q96')
+  get_ipython().system_raw('unzip -q /content/pretrain_Q96.zip -d /content/pretrain_Q96/')
+  get_ipython().system_raw('rm /content/pretrain_Q96.zip')
 
 if not Path("/content/workspace").exists():
-  !mkdir /content/workspace; mkdir /content/workspace/data_src; mkdir /content/workspace/data_src/aligned; mkdir /content/workspace/data_dst; mkdir /content/workspace/data_dst/aligned; mkdir /content/workspace/model  
+  get_ipython().system_raw('mkdir /content/workspace; mkdir /content/workspace/data_src; mkdir /content/workspace/data_src/aligned; mkdir /content/workspace/data_dst; mkdir /content/workspace/data_dst/aligned; mkdir /content/workspace/model')  
 
 import IPython
 from google.colab import output
@@ -74,9 +74,9 @@ setInterval(ClickConnect,60000)
 '''))
 
 print("\nDone!")
-!wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-!unzip ngrok-stable-linux-amd64.zip
+get_ipython().system_raw('wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip')
+get_ipython().system_raw('unzip ngrok-stable-linux-amd64.zip')
 
 
-!./ngrok authtoken 5vhWvAzJGtsJbnVp4V5di_6KNVTN8BpHMqKYyAaFFXQ
+get_ipython().system_raw('./ngrok authtoken 5vhWvAzJGtsJbnVp4V5di_6KNVTN8BpHMqKYyAaFFXQ')
 print("Done!")
